e0773286db387ab51edbd81f8f6b32b4633b348a
JulianaVargas309 <julianavidal309@gmail.com>
2026-05-07 17:31:50 -0500

    cambio de submodulos de ubicación

--- src/core/api/httpEfastack.js
+++ src/core/api/httpEfastack.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const httpEfaStack = axios.create({
-    baseURL: import.meta.env.VITE_EFASTACK_API_URL,
+  baseURL: import.meta.env.VITE_EFASTACK_API_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 export default httpEfaStack;
