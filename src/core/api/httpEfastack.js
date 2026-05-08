7e802744e062de8cd4d81a5ee5034a5d33f7146d
JulianaVargas309 <julianavidal309@gmail.com>
2026-05-08 09:10:08 -0500

    cambios necesarios en js para el responsivo

--- src/core/api/httpEfastack.js
+++ src/core/api/httpEfastack.js
@@ -1,10 +1,21 @@
 import axios from 'axios';
+import { getToken } from '../auth/tokenStorage';
 
 const httpEfaStack = axios.create({
   baseURL: import.meta.env.VITE_EFASTACK_API_URL,
   headers: {
     'Content-Type': 'application/json'
   }
+});
+
+httpEfaStack.interceptors.request.use((config) => {
+  const token = getToken();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default httpEfaStack;
